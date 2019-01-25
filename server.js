f1e1053a7c7c2c0203840a9b8e6d30507aba3ff1
Kurt King <kurtking@Kurts-MacBook-Pro.local>
2019-01-24 18:07:05 -0600

    updates

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
+var router = require("./controllers/burgers_controller")
+
 
 var app = express(); 
 
 var PORT = process.env.PORT || 8080;
 
-
+app.use(router);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
